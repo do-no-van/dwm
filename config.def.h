0fc682188ab8656050a8e0657ce9a490b6b8485b
do-no-van <47617272696e@gmail.com>
2022-01-24 16:16:36 -0600

    Patch swallow

--- config.def.h
+++ config.def.h
@@ -3,19 +3,20 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static unsigned int borderpx  = 1;        /* border pixel of windows */
-static unsigned int snap      = 32;       /* snap pixel */
-static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
-static char font[]            = "monospace:size=10";
-static char dmenufont[]       = "monospace:size=10";
-static const char *fonts[]          = { font };
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
+static unsigned int borderpx     = 1;        /* border pixel of windows */
+static unsigned int snap         = 32;       /* snap pixel */
+static const int swallowfloating = 0;        /* 1 means swallow floating windows by default */
+static int showbar               = 1;        /* 0 means no bar */
+static int topbar                = 1;        /* 0 means bottom bar */
+static char font[]               = "monospace:size=10";
+static char dmenufont[]          = "monospace:size=10";
+static const char *fonts[]       = { font };
+static char normbgcolor[]        = "#222222";
+static char normbordercolor[]    = "#444444";
+static char normfgcolor[]        = "#bbbbbb";
+static char selfgcolor[]         = "#eeeeee";
+static char selbordercolor[]     = "#005577";
+static char selbgcolor[]         = "#005577";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
@@ -30,9 +31,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "st-256color",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
